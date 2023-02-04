4b85afa678328ce0ca07972408c436cc8454ac63
William Kristiawan <williamkristiawan38@gmail.com>
2023-02-04 08:55:39 +0700

    Add all my files

---
+++ models/customer.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Customer extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.hasMany(models.Books,{
+        as:'books',
+        foreignKey: 'customer_id'
+      })
+    }
+  }
+  Customer.init({
+    email: DataTypes.STRING,
+    name: DataTypes.STRING,
+    address: DataTypes.STRING,
+    phone_number: DataTypes.INTEGER,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Customers',
+    tableName: 'customers'
+  });
+  return Customer;
+};
